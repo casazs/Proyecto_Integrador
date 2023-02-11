8c75d77ce25e49d5a07a953aa9d27e6f80b08fc8
Mateo Casas <mateo2casas@gmail.com>
2023-02-11 16:28:45 -0300

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArPrComponent } from './components/logo-ar-pr/logo-ar-pr.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
-import { ExperienceComponent } from './components/experience/experience.component';
+import { ExperienciaComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import { SkillsComponent } from './components/skills/skills.component';
+import { HysComponent } from './components/hys/hys.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import{HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { NewExperienciaComponent } from './components/experience/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experience/edit-experiencia/edit-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -27,15 +29,19 @@ import { LoginComponent } from './components/login/login.component';
     ExperienceComponent,
     EducationComponent,
     SkillsComponent,
+  
     FooterComponent,
     ProyectsComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
